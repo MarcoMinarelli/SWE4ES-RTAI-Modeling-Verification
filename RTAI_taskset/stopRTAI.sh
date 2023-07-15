4d31505ba2f59b2cfb0e4f95338f0262502e3952
MarcoMinarelli <39798373+MarcoMinarelli@users.noreply.github.com>
2023-07-15 16:12:11 +0200

    Corrected taskset, added useful files for running the taskset

---
+++ RTAI_taskset/stopRTAI.sh
@@ -0,0 +1,7 @@
+#! /bin/sh
+sudo rmmod /usr/realtime/modules/rtai_mbx.ko
+sudo rmmod /usr/realtime/modules/rtai_sem.ko
+sudo rmmod /usr/realtime/modules/rtai_fifos.ko
+sudo rmmod /usr/realtime/modules/rtai_sched.ko
+sudo rmmod /usr/realtime/modules/rtai_hal.ko
+
